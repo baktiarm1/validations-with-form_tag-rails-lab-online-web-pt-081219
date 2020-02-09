daa70219887eef09d5653c9a33d874d65c345683
Baktiar Miah <baktiar.m@gmail.com>
2020-02-09 23:41:13 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,17 @@ class PostsController < ApplicationController
 
   def new
     @author= Post.new
-  end 
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
 
   def edit
     @post = Post.find(params[:id])
